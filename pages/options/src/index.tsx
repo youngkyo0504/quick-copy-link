9258fc6b0490eda1412b807d6ab4d8ad47858e73
keumky2 <keumky2@woowahan.com>
2024-07-09 18:08:44 +0900

    radix theme 적용

--- pages/options/src/index.tsx
+++ pages/options/src/index.tsx
@@ -1,14 +1,22 @@
 import { createRoot } from 'react-dom/client';
 import '@src/index.css';
 import Options from '@src/Options';
+import '@radix-ui/themes/styles.css';
+import { Theme } from '@radix-ui/themes';
 
 function init() {
   const appContainer = document.querySelector('#app-container');
   if (!appContainer) {
     throw new Error('Can not find #app-container');
   }
+
   const root = createRoot(appContainer);
-  root.render(<Options />);
+
+  root.render(
+    <Theme>
+      <Options />
+    </Theme>,
+  );
 }
 
 init();
